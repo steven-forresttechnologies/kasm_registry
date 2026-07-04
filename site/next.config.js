e673e8b8f872b394e18f9da2b2c641557c2914c1
steven-forresttechnologies <steven@forresttechnologies.com>
2026-07-03 22:43:57 -0400

    Update Kasm Registry environment variables

--- site/next.config.js
+++ site/next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: "Teja's Kasm Registry",
-    description: 'My Custom Images for Kasm Workspaces',
+    name: "Forrest Technologies Kasm Registry",
+    description: 'Custom Images for Kasm Workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.tejaswaroop.com/',
+    listUrl: 'https://steven-forresttechnologies.github.io/kasm_registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
